c3ce9dcdb19eb939e4d13218d8a650f8bd182b11
Robert A Fraser <robertadinfraser@gmail.com>
2022-04-18 14:58:26 +1000

    Add link to game in ending message

--- src/components/CompletionMessage.vue
+++ src/components/CompletionMessage.vue
@@ -2,17 +2,27 @@
     <div v-if="gameOver" class="my-2 text-white text-center">
         <p v-if="hasWon">You got it!</p>
         <p v-else-if="gameOver">Oh no! The answer was <b>{{ gameDetails.title }}</b></p>
+
+        <p>The answer was <a class="underline" :href="gameUrl">{{ gameDetails.title }}</a></p>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { gameOver, hasWon } from '../store';
+import { gameOver, hasWon, store } from '../store';
 
 export default defineComponent({
   name: 'CompletionMessage',
 
   computed: {
+    gameDetails() {
+      return store.gameDetails;
+    },
+
+    gameUrl(): string {
+      return `https://store.steampowered.com/app/${this.gameDetails.steamId}`;
+    },
+
     gameOver() {
       return gameOver();
     },
--- src/store.ts
+++ src/store.ts
@@ -8,6 +8,7 @@ export const store = reactive({
     description: '',
     title: '',
     releaseYear: '',
+    steamId: '',
   },
 });
 
